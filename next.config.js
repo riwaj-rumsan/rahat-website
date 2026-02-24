fd83579513a06e827d6ea98df39953ee21cf1783
Riwaj Chipalu <riwajchipalu@gmail.com>
2026-02-24 17:08:24 +0545

    fix static export image config

--- next.config.js
+++ next.config.js
@@ -1,55 +1,58 @@
 const path = require("path");
 
-module.exports = {
-    reactStrictMode: true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, "src/assets/scss")],
-    },
-    images: {
-        domains: [
-            "drive.google.com",
-            "rumsan.nyc3.cdn.digitaloceanspaces.com",
-            "rumsan.nyc3.cdn.digitaloceanspaces.com",
-            "rahat-rumsan.s3.amazonaws.com",
-            "esatya.s3.amazonaws.com",
-            "assets.rumsan.net",
-            "rahat-rumsan.s3.us-east-1.amazonaws.com",
-            "rahat-rumsan.s3.us-east-1.amazonaws.com",
-            "rahat.io",
-            "assets.rumsan.net",
-            "rahat",
-            "next",
-        ],
-    },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // Ignore specific modules to avoid unnecessary warnings
-        config.ignoreWarnings = [
-            ...(config.ignoreWarnings || []),
-            /magic-sdk/,
-            /@walletconnect[\\/]web3-provider/,
-            /@web3auth[\\/]web3auth/,
-            /@walletconnect[\\/]universal-provider/,
-        ];
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 
-        // Exclude 'react-native' from resolving for @walletconnect/universal-provider
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            reactNative: "react-native-web",
-        };
+  // ⚠ If this is frontend only, use export instead of standalone
+  output: "export",
 
-        return config;
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/assets/scss")],
+  },
+
+  images: {
+    domains: [
+      "drive.google.com",
+      "rumsan.nyc3.cdn.digitaloceanspaces.com",
+      "rahat-rumsan.s3.amazonaws.com",
+      "esatya.s3.amazonaws.com",
+      "assets.rumsan.net",
+      "rahat-rumsan.s3.us-east-1.amazonaws.com",
+      "rahat.io",
+    ],
+  },
+
+  webpack: (config, { isServer }) => {
+    // Disable persistent webpack cache (fixes 318MB issue)
+    config.cache = false;
+
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      /magic-sdk/,
+      /@walletconnect[\\/]web3-provider/,
+      /@web3auth[\\/]web3auth/,
+      /@walletconnect[\\/]universal-provider/,
+    ];
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      reactNative: "react-native-web",
+    };
+
+    return config;
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  rewrites: async () => [
+    {
+      source: "/cookie-policy",
+      destination: "/cookie-policy.html",
     },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
-    rewrites: async () => {
-        return [
-            {
-                source: "/cookie-policy",
-                destination: "/cookie-policy.html",
-            },
-        ];
-    },
+  ],
 };
+
+module.exports = nextConfig;
